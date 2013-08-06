477c954d757da10f6bbbfad1a49165a6b5aee9cc
Weston Platter <westonplatter@gmail.com>
2013-08-06 13:20:25 -0700

    heroku does "Piggyback" SSL by default, therefore, don't need to turn off SSL (/admin still works fine).

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,13 +9,6 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
-  
-  
-  # Set SSL to false so we don't have to purchase heroku SSL addon. If this 
-  # site were real (not a demo site), then we would NOT disable SSL due to 
-  # Man-in-the-Middle attacks.
-  # 
-  config.set(:allow_ssl_in_production => false)
 end
 
 Spree.user_class = "Spree::LegacyUser"
